6cfa4994972e83ce229a1a783f26282a65ecfe18
Aleksy Dowejko <aleks6566@wp.pl>
2023-11-19 20:05:09 +0100

    Dzień Czwarty

---
+++ ChallengeApp/ChallengeApp/ChallegeApp/ChallegeApp/Program.cs
@@ -0,0 +1,22 @@
+﻿
+// Dzień Czwarty
+string name = "Ewa";
+var isWomen = "true";
+int age = 33;
+
+if (isWomen == "true" && age < 30)
+{
+    Console.WriteLine("Kobieta Poniżej 30 lat");
+}
+else if (isWomen == "true" && age == 33 && name == "Ewa")
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (isWomen == "false" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine($"Osoba o imieniu: {name} w wieku: {age} lat");
+}
