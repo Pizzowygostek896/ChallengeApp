701157088dfffd529793abae4a2280d44366c1b3
Aleksy Dowejko <aleks6566@wp.pl>
2023-11-28 09:22:14 +0100

    zień 7

---
+++ ChallengeApp7/ChallengeApp.7.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+using ChallengeApp7.Tests;
+
+namespace ChallengeApp7.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            // arrange
+            var employee = new Employee("Robert", "aliucbwc");
+            employee.AddScore(7);
+            employee.AddScore(9);
+
+            // act
+            int result = employee.Result;
+
+            //assert
+            Assert.AreEqual(16, result);
+        }
+    }
+}
