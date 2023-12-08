c6a529f301d02b291b071adadab3c502fd9a3c71
Aleksy Dowejko <aleks6566@wp.pl>
2023-12-08 21:15:44 +0100

    Dzień 9

---
+++ ChallengeApp9/ChallengeApp9/Program.cs
@@ -0,0 +1,23 @@
+﻿using ChallengeApp9;
+ 
+
+    var employee = new Employee("Aleksy", "Dowejko");
+    employee.AddGrade(8);
+    employee.AddGrade(5);
+    employee.AddGrade(6);
+    var stat = employee.GetStatistics();
+    Console.WriteLine($"Average: {stat.Average:N2}");
+    Console.WriteLine($"Min: {stat.Min}");
+    Console.WriteLine($"Max: {stat.Max}");
+
+    SetSth(out stat);
+
+
+    void SetSth(out Statistics stat)
+    {
+        stat = new Statistics();
+    }
+
+   
+
+
---
+++ ChallengeApp9/ChallengeApp9/Statistics.cs
@@ -0,0 +1,14 @@
+﻿namespace ChallengeApp9
+{
+    public class Statistics
+    {
+        public float Min { get; set; }
+
+        public float Max { get; set; }
+
+
+        public float Average { get; set; }
+        public object? MaxValue { get; set; }
+        public object? MinValue { get; set; }
+    }
+}
